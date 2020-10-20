4ec90b33166544799f4ece09465a0a6d965715c7
Helvin <hjwc86@gmail.com>
2020-10-20 14:10:02 +0200

    événements

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Card from './Card'
 import GuessCount from './GuessCount'
 
 class App extends Component {
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
   render() {
     return (
       <div className="memory">
         <GuessCount guesses={0} />
         {/*La carte est masquée */}
-        <Card card="😀" feedback="hidden" />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
         {/* La carte fait partie de la tentative en cours, qui vient de réussir une paire */}
-        <Card card="🎉" feedback="justMatched" />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
         {/* La carte fait partie de la tentative en cours, qui vient de rater une paire */}
-        <Card card="💖" feedback="justMismatched" />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
         {/* La carte appartient à une paire précédemment réussie */}
-        <Card card="🎩" feedback="visible" />
-        <Card card="🐶" feedback="hidden" />
-        <Card card="🐱" feedback="justMatched" />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
       </div>
     )
   }
--- src/Card.js
+++ src/Card.js
@@ -4,9 +4,9 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback }) => (
+const Card = ({ card, feedback, onClick }) => (
 
-    <div className={`card ${feedback}`}>
+    <div className={`card ${feedback}`} onClick={()=>onClick(card)}>
         <span className="symbol">
             {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
         </span>
